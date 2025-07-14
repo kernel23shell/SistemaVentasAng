5d1c5525b7db328d1e66e9105937824a776c4d35
kernel23shell <shell.kernel23@gmail.com>
2025-07-14 13:13:26 -0600

    Se agregan repositorios

--- SistemaVenta.DAL/DBContext/DbventaContext.cs
+++ SistemaVenta.DAL/DBContext/DbventaContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using SistemaVenta.Model;
 
 namespace SistemaVenta.DAL.DBContext;
 
@@ -33,9 +34,7 @@ public partial class DbventaContext : DbContext
 
     public virtual DbSet<Ventum> Venta { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-V4645N3\\SQLEXPRESS;Initial Catalog=DBVENTA;Integrated Security=True;Encrypt=False");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ SistemaVenta.DAL/Repositorios/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Repositorios
+{
+    public class GenericRepository<TModelo> :IGenericRepository<TModelo> where TModelo : class
+
+    {
+        private readonly DbventaContext _dbcontext;
+
+        public GenericRepository(DbventaContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public Task<IQueryable<TModelo>> Consultar(Expression<Func<TModelo, bool>> filtro = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TModelo> Crear(TModelo modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Editar(TModelo modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(TModelo modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TModelo> Obtener(Expression<Func<TModelo, bool>> filtro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
