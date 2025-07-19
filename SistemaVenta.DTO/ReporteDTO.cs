ac7c5b7cdf90c9b302e8fd95ba0b85c879942493
kernel23shell <shell.kernel23@gmail.com>
2025-07-18 22:27:40 -0600

    se agregan contratos y servicios en BLL parte01

---
+++ SistemaVenta.DTO/ReporteDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.DTO
+{
+    public class ReporteDTO
+    {
+        public string? NumeroDocumento { get; set; }
+        public string? TipoPago { get; private set; }
+        public string? FechaRegistro { get; private set; }
+        public string? TotalVenta { get; private set; }
+        public string? Producto { get; private set; }
+        public int? Cantidad { get; private set; }
+        public string? Precio { get; private set; }
+        public string? Total { get; private set; }
+    }
+}
